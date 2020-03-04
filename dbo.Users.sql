f01478497d2e0989f512f61f2e146c0b767c4952
10505969 <59395477+10505969@users.noreply.github.com>
2020-03-04 14:12:02 +0000

    Delete dbo.Users.sql

--- dbo.Users.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Users] (
-    [Id]       INT           IDENTITY (1, 1) NOT NULL,
-    [UserName] VARCHAR (255) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
-    [Pword]    VARCHAR (255) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
